874a15e62c55acb72598ceeb5ae5c62066fb32c0
Iago Leonardo Laguna <iagolaguna.83@gmail.com>
2017-09-28 19:22:36 -0300

    intial structure

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import HttpStatus from 'http-status';
+// import Router from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+// app.use('/api', cors({
+// 	origin: false
+// }), Router);
+
+app.set('port', process.env.port || process.env.PORT || 80);
+
+app.use(function (request, response, next) {
+	let err = new Error('Not Found');
+	err.status = HttpStatus.NOT_FOUND;
+	next(err);
+});
+
+app.use(function (err, request, response) {
+	response
+		.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR)
+		.json({
+			err: err.message
+		});
+});
+
+export default app;
